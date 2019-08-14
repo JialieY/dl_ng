9f8997394785f02bde3bebd9cdc2f69c22137cbc
JialieY <35982326+JialieY@users.noreply.github.com>
2019-08-14 11:47:01 -0700

    Create face.py

---
+++ Course4/Week4/Face/face.py
@@ -0,0 +1,116 @@
+
+from keras.models import Sequential
+from keras.layers import Conv2D, ZeroPadding2D, Activation, Input, concatenate
+from keras.models import Model
+from keras.layers.normalization import BatchNormalization
+from keras.layers.pooling import MaxPooling2D, AveragePooling2D
+from keras.layers.merge import Concatenate
+from keras.layers.core import Lambda, Flatten, Dense
+from keras.initializers import glorot_uniform
+from keras.engine.topology import Layer
+from keras import backend as K
+K.set_image_data_format('channels_first')
+import cv2
+import os
+import numpy as np
+from numpy import genfromtxt
+import pandas as pd
+import tensorflow as tf
+from fr_utils import *
+from inception_blocks_v2 import *
+
+FRmodel = faceRecoModel(input_shape=(3, 96, 96))
+print(FRmodel.summary())
+print("Total Params:", FRmodel.count_params())
+
+
+# define triplet loss
+def triplet_loss(y_true, y_pred, alpha = 0.2):
+    anchor, positive, negative = y_pred[0], y_pred[1], y_pred[2]
+    
+    # Step 1: Compute the (encoding) distance between the anchor and the positive, you will need to sum over axis=-1
+    pos_dist =  tf.reduce_sum(tf.square(tf.subtract(anchor,positive)),axis=-1)
+    # Step 2: Compute the (encoding) distance between the anchor and the negative, you will need to sum over axis=-1
+    neg_dist =  tf.reduce_sum(tf.square(tf.subtract(anchor,negative)),axis=-1)
+    # Step 3: subtract the two previous distances and add alpha.
+    basic_loss =  pos_dist - neg_dist + alpha
+
+    # Step 4: Take the maximum of basic_loss and 0.0. Sum over the training examples.
+    loss = tf.reduce_sum(tf.maximum(0.0,basic_loss))
+    return loss
+
+# If you want, you can train this model, here we just load pre-trained weights
+FRmodel.compile(optimizer = 'adam', loss = triplet_loss, metrics = ['accuracy'])
+load_weights_from_FaceNet(FRmodel)
+
+
+database = {}
+database["danielle"] = img_to_encoding("images/danielle.png", FRmodel)
+database["younes"] = img_to_encoding("images/younes.jpg", FRmodel)
+database["tian"] = img_to_encoding("images/tian.jpg", FRmodel)
+database["andrew"] = img_to_encoding("images/andrew.jpg", FRmodel)
+database["kian"] = img_to_encoding("images/kian.jpg", FRmodel)
+database["dan"] = img_to_encoding("images/dan.jpg", FRmodel)
+database["sebastiano"] = img_to_encoding("images/sebastiano.jpg", FRmodel)
+database["bertrand"] = img_to_encoding("images/bertrand.jpg", FRmodel)
+database["kevin"] = img_to_encoding("images/kevin.jpg", FRmodel)
+database["felix"] = img_to_encoding("images/felix.jpg", FRmodel)
+database["benoit"] = img_to_encoding("images/benoit.jpg", FRmodel)
+database["arnaud"] = img_to_encoding("images/arnaud.jpg", FRmodel)
+
+
+# Face Verification
+def verify(image_path, identity, database, model):
+    # Step 1: Compute the encoding for the image. Use img_to_encoding() see example above. (≈ 1 line)
+    encoding = img_to_encoding(image_path, model)
+    # Step 2: Compute distance with identity's image (≈ 1 line)
+    dist = np.linalg.norm(database[identity]-encoding)
+    # Step 3: Open the door if dist < 0.7, else don't open (≈ 3 lines)
+    if dist < 0.7:
+        print("It's " + str(identity) + ", welcome home!")
+        door_open = True
+    else:
+        print("It's not " + str(identity) + ", please go away")
+        door_open = False
+
+    return dist, door_open
+
+
+verify("images/camera_0.jpg", "younes", database, FRmodel)
+# It's younes, welcome home!
+# (0.65939283, True)
+
+verify("images/camera_2.jpg", "kian", database, FRmodel)
+# It's not kian, please go away
+# (0.86224014, False)
+
+# Face Recognition
+def who_is_it(image_path, database, model):
+    ## Step 1: Compute the target "encoding" for the image. Use img_to_encoding() see example above. ## (≈ 1 line)
+    encoding = img_to_encoding(image_path, model)
+    
+    ## Step 2: Find the closest encoding ##
+    # Initialize "min_dist" to a large value, say 100 (≈1 line)
+    min_dist = 100
+    
+    # Loop over the database dictionary's names and encodings.
+    for (name, db_enc) in database.items():
+        # Compute L2 distance between the target "encoding" and the current "emb" from the database. (≈ 1 line)
+        dist = np.linalg.norm(db_enc-encoding)
+        # If this distance is less than the min_dist, then set min_dist to dist, and identity to name. (≈ 3 lines)
+        if dist < min_dist :
+            min_dist = dist
+            identity = name
+
+    # Check the minimum distance between the new image and database
+    if min_dist > 0.7:
+        print("Not in the database.")
+    else:
+        print ("it's " + str(identity) + ", the distance is " + str(min_dist))
+        
+    return min_dist, identity
+
+who_is_it("images/camera_0.jpg", database, FRmodel)
+
+# it's younes, the distance is 0.659393
+# (0.65939283, 'younes')
